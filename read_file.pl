60ba075e63681c2b71a28d2ae80d26457026fc71
Sdriver1 <sjdriver08@gmail.com>
2024-07-22 00:01:44 -0400

    First Program

---
+++ read_file.pl
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+
+my $filename = 'example.txt';
+open(my $fh, '<:encoding(UTF-8)', $filename)
+  or die "Could not open file '$filename' $!";
+
+while (my $row = <$fh>) {
+    chomp $row;
+    print "$row\n";
+    }
+
+close $fh;
